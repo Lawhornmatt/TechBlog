7cc327733b1818d2fe7e62b4a1e2dd7d953acf21
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-10-10 16:01:27 -0500

    Basic Post Layout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const {Generic} = require('../models');
 const bcrypt = require('bcrypt')
 
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     res.render('home', {
       logged_in: req.session.logged_in,
