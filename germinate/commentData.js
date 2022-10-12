2974fa8a9f2cb6c7cb0514c9999bc45007464f42
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-10-12 12:12:15 -0500

    Visibility & Seeding
    Made all post headers the lighter color and posts by the logged in user a bright yellow. I think this gives the right amount of pop and makes things much more legible. Also fixed some weird seeding by not specifying the post IDs. Easy fix, glad I didnt have to redive into the route guts.

--- germinate/commentData.js
+++ germinate/commentData.js
@@ -11,6 +11,26 @@ const commentData = [
         postid: 1,
         commentbody: "Good afternoon, sir/madam. What a marvelous post! Truly incredible, an exemplar of our wonderous internet community. Please continue to grace us with your wisdom on this internet forum. Comment 2"
     },
+    { //3
+        userid: 1,
+        postid: 1,
+        commentbody: "I am OP; I am commenting on my own post, hello"
+    },
+    { //4
+        userid: 2,
+        postid: 2,
+        commentbody: "I am OP; I am commenting on my own post, hello"
+    },
+    { //5
+        userid: 1,
+        postid: 2,
+        commentbody: "OP is a dum"
+    },
+    { //6
+        userid: 2,
+        postid: 1,
+        commentbody: "No u"
+    },
 ];
 
 const seedComment = () => Comment.bulkCreate(commentData, {
--- germinate/userData.js
+++ germinate/userData.js
@@ -1,12 +1,14 @@
 const { User } = require('../models');
 
 const userData = [
-    { //1
+    { 
+        id: 1,
         username: 'JohnnyMo1',
         email: "nuclear@test.com",
         password: "password"
     },
-    { //2
+    {
+        id: 2,
         username: 'Hezzy',
         email: "hezzy@test.com",
         password: "password"
