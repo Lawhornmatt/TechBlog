6dc5b7568cf30128fb4d1fd942a5d0fbbbcb6ba0
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-10-12 16:21:55 -0500

    No Icon, Fix LogOut

--- public/libs/logout.js
+++ public/libs/logout.js
@@ -3,16 +3,12 @@
   // ====================
 
 const logout = async () => {
-  const response = await fetch('/logout', {
+  await fetch('/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
-
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert('Log out failed');
-  }
+  
+  document.location.replace('/');
 };
 
   // ====================
